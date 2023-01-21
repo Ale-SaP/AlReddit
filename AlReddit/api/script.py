abdd8cb7f3bedc3c2b7378137b023bc1309de5b1
Ale-SaP <85176998+Ale-SaP@users.noreply.github.com>
2023-01-21 17:17:13 -0300

    Added .env files; script.py creates a working link.

---
+++ AlReddit/api/script.py
@@ -0,0 +1,18 @@
+import praw
+
+import environ
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
+
+import random
+
+reddit = praw.Reddit(
+    client_id = env("CLIENT_ID"),
+    client_secret = env("SECRET_KEY"),
+    redirect_uri = env("REDIRECT_URI"),
+    user_agent = env("USER_AGENT"),
+)
+
+state = str(random.randint(0, 65000))
+print(reddit.auth.url(scopes=["account"], state=state, duration="permanent"))
